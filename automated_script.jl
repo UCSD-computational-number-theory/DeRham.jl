91d4897e87f915068fcc43cb3e23f8c1a292cc2f
MaximusMellberg <156002603+MaximusMellberg@users.noreply.github.com>
2024-01-13 16:04:20 -0800

    Finished most of the main script
    Still need to account for edge cases and decide how we want to format our input

--- automated_script.jl
+++ automated_script.jl
@@ -9,11 +9,12 @@ using Oscar
 
 # TODO: Modify all the below to work in Fp.
 
-n = 2
+n = 3
 p = 2
 Fp = GF(p)
 
-R, (x, y, z) = PolynomialRing(ZZ, ["x$i" for i in 1:n])
+R = ZZ
+PR, (x, y, z) = PolynomialRing(R, ["x$i" for i in 1:n])
 polynomial = y^2*z - x^3 - x*z^2 - z^3
 
 # TODO: Check that it is homogenous. Let d = degree.
@@ -23,6 +24,30 @@ d = 3
 partials = [ derivative(polynomial, i) for i in 1:n ]
 
 # TODO: Ensure partials are not all zero.
+
+# Generates vector of exponentials
+function gen_exp_vec(n,d)
+    result = Any[]
+    if n == 1
+        return [[d]]
+    end
+    if d == 1
+        for i in 1:n
+            s = zeros(Int64,n)
+            s[i] = 1
+            push!(result,s)
+        end
+        return result
+    end
+    for i in 0:d
+        y = gen_exp_vec(n-1,d-i)
+        for j in axes(y,1)
+            append!(y[j],i)
+        end
+        append!(result,y)
+    end
+    return result
+end
 
 # Computes all monomials of degree `d` in `n` variables.
 function compute_monomials(n, d)
@@ -46,31 +71,69 @@ function compute_monomials(n, d)
     return result
 end
 
+# Computes the relations
+function compute_relations(monomials, partials)
+    result = []
+    for i in axes(monomials,1)
+        for j in axes(partials,1)
+            push!(result, monomials[i]*partials[j])
+        end
+    end
+    return result
+end
+
+# Converts vector of homogeneous polynomials to a matrix of their coefficents
+function convert_p_to_m(polys, expvec)
+    result = []
+    for i in axes(polys,1)
+        temp = []
+        for j in axes(expvec,1)
+            append!(temp, [coeff(polys[i], expvec[j])])
+        end
+        push!(result, transpose(temp))
+    end
+    return vcat(result...)
+end
+
+# Converts Matrix of coefficents to vector of polynomials, each row is one polynomial
+function convert_m_to_p(mat, expvec, R, PR)
+    result = []
+    for i in axes(mat,1)
+        B = MPolyBuildCtx(PR)
+        for j in axes(expvec,1)
+            push_term!(B, mat[i,j], expvec[j])
+        end
+        push!(result,finish(B))
+    end
+    return result
+end
+
 # Computes the basis vectors associated with case h. Columns of
 # returned matrix will be linearly independent vectors.
-function basis_vectors(h)
+function basis_vectors(n, d, h, partials, R)
     # If number of monomials is too small, just use
     # constant as basis vector.
     if h*d - n - 1 <= 0
         return R(1)
+    end
 
     # compute all monomials of degree `hd - n - 1`
-    monomials = compute_monomials(n + 1, h*d - n - 1)
+    expvec = gen_exp_vec(n+1, h*d - n - 1)
 
     # TODO: compute all distinct products between the partial derivatives
-    # and monomials(n+1, hd - n - d - 1). These are our relations.
+    # and monomials(n+1, hd - n - d). These are our relations.
+    rmonomials = compute_monomials(n+1, h*d - n - d)
+    relations = compute_relations(rmonomials, partials)
 
     # TODO: Check that the number of relations is <= the number of
     # monomials (which is = n + d - 1 choose d)
 
     # TODO: Let the i-th vector element of `monomials` be the i-th basis
     # element in the monomial basis. Convert all relations into that form.
-    vectors = []
-
-    # Compute the nullspace of the matrix created via the above row vectors
-    M = vcat(vectors...)
+    M = matrix(R, convert_p_to_m(relations, expvec))
 
     return nullspace(M)
+end
 
 # TODO: Compute Omega in terms of differential symbol and exterior
 # products (perhaps as symbols).
