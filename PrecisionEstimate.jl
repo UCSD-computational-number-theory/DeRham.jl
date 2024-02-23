277c1b2760587c66b5372f0b7ff57f589c9652e4
ThomasSpradling <thomasspradling13@gmail.com>
2024-02-23 10:02:25 -0800

    feat: combined controlled and uncontrolled steps

--- PrecisionEstimate.jl
+++ PrecisionEstimate.jl
@@ -1,18 +1,18 @@
 module PrecisionEstimate
 
-p = 7
-r = 12 # precision
-n = 4
+p = 41
+r = 7 # precision
+n = 2
 
 function compute_N(p, r, m, n)
     num = n * log(n + r)
     denom = (n + r) * log(p) - n
-    return ceil(Int, -m + (n + r) * (1 + num / denom)) # Convert the result to an integer
+    return ceil(Int, -m + (n + r) * (1 + num / denom))
 end
 
 function compute_precisions_each(p, r, n)
-    for i = 1:n
-        println(compute_N(p, r, i, n)) # Change Printf to println
+    for m = 1:n
+        println(compute_N(p, r, m, n))
     end
 end
 
