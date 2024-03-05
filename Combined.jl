e746a65cc06d0e87f69fc3a50e2a0e1236e4925e
Youran-Geng <89507458+Youran-Geng@users.noreply.github.com>
2024-03-05 14:41:33 -0800

    w

--- Combined.jl
+++ Combined.jl
@@ -5,7 +5,15 @@ include("ControlledReduction.jl")
 
 
 function frobenius_matrix_classical(f, n, d, precision, p)
+    # Step 1: find monomial basis
+    PR = parent(f)
+    R = coefficient_ring(PR) # function name?
+    
+    # Step 2: apply Frobenius
 
+    # Step 3: reduction
+
+    # Step 4: process the answer
 end
 
 function frobenius_matrix_controlled(f, n, d, precision, p)
