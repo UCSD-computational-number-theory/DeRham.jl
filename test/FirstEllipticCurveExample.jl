35c7f87f8a9a8cde7af7baf8c99524c877bcf26a
MaximusMellberg <maximusmell33@gmail.com>
2024-09-08 19:35:07 -0700

    Fixed some tests

--- test/FirstEllipticCurveExample.jl
+++ test/FirstEllipticCurveExample.jl
@@ -193,14 +193,14 @@ function testRedOfTerms()
             push!(Basis,[j,i])
         end
     end
-    FBasis = DeRham.applyFrobeniusToBasis(Basis, fLift, N, p, :invlex)
+    FBasis = DeRham.applyFrobeniusToBasis(Basis, fLift, N, p, :invlex,true)
     #pseudoInverseMat = zeros(PrecisionRing,nrows(pseudoInverseMatTemp),ncols(pseudoInverseMatTemp))
     #for i in 1:nrows(pseudoInverseMat)
     #    for j in 1:ncols(pseudoInverseMat) 
     #        pseudoInverseMat[i,j] = PrecisionRing(lift(ZZ,pseudoInverseMatTemp[i,j]))
     #    end
     #end
-    Reductions = DeRham.reducetransform(FBasis, N, S, fLift, matrix(PrecisionRing,pseudo_inverse_mat), p, :invlex, :costachunks)
+    Reductions = DeRham.reducetransform(FBasis, N, S, fLift, matrix(PrecisionRing,pseudo_inverse_mat), p, :invlex, :costachunks,true)
     ev = DeRham.gen_exp_vec(n+1,n*d-n-1,:invlex)
     reductions_as_rows = DeRham.convert_p_to_m([Reductions[1][1][1],Reductions[2][1][1]],ev)
     RR = parent(reductions_as_rows[1,1])
