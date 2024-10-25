cadc476a6c87c55cd5d76fff7d59d7dbaf999e1f
JJ Garzella <jjgarzella@gmail.com>
2024-10-25 07:55:14 -0700

    Add smoothness check
    Add method that checks smoothness.
    It simply wraps Oscar's functionality.

--- src/ZetaFunction.jl
+++ src/ZetaFunction.jl
@@ -314,6 +314,31 @@ function hodgepolygon(f; termorder=:invlex)
     hodgepolygon(Basis)
 end
 
+
+"""
+    check_smoothness(f)
+
+Using Oscar, check whether f defines a smooth hypersurface.
+
+f is assumed to be homogeneous already. Otherwise Oscar
+will throw an error.
+
+note: the name issmooth / is_smooth is already taken by oscar,
+and really we're just wrapping that method.
+"""
+function check_smoothness(f)
+    p = characteristic(parent(f))
+    nVars = length(gens(parent(f)))
+
+    graded, _ = grade(parent(f))
+
+    R, _ = quo(graded, ideal(graded, [graded(f)]))
+
+    V = proj(R)
+
+    is_smooth(V)
+end
+
 #end 
 
 #=
