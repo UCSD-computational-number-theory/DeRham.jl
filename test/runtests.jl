34c3df5052326d8b5c75e685ace2ccf698ca2d74
JJ Garzella <jjgarzella@gmail.com>
2025-04-14 10:43:36 -0700

    Remove failing tests that now work
    Put them in their correct test files
    
    Also add the examples which need BigInts
    (using the current precision estimates)

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,13 +24,13 @@ include("ManageCSVTests.jl")
     #testFrobMat()
 end
 
-function larger_tests()
-    zf = f -> DeRham.zeta_function(f,algorithm=:naive,fastevaluation=true)
+# currently, this runs on all the examples that can be done with a full S
+function larger_tests(zf)
 
     # Keep track of which files to run with (dim,degree) tuples
     fermatfiles = [(1,3), (1,4), (1,5), (1,6), (1,7), (1,8),
                    (2,4), (2,5)]
-    randomfiles = [(1,3), (1,4), (1,5), (1,6), (2,4)]
+    randomfiles = [(1,3), (1,4), (1,5), (1,6), (1,7), (2,4)]
     manyfiles = [(1,3),(1,4),(1,5)]
     
     for (dim,deg) in fermatfiles
@@ -122,6 +122,16 @@ end
     end
 
     #TODO: insert specific examples that need the use of bigints here
+
+    # No BigInts necessary
+    teststring("3;47;x1^6 + x2^6 + x3^6;ZZRingElem[52599132235830049,0,11191304731027670,0,1071507899779245,0,60794774455560,0,2263635219090,0,57794941764,0,1024733010,0,12458760,0,99405,0,470,0,1]",zf)
+
+    # Needs UInt64, not Int64
+    teststring("3;53;x1^6 + x2^6 + x3^6;ZZRingElem[174887470365513049,0,32997635918021330,0,2801686068511245,0,140965336780440,0,4654515837090,0,105385264236,0,1657001010,0,17865240,0,126405,0,530,0,1
+]",zf)
+
+    # Needs BigInts
+    teststring("3;59;x1^6 + x2^6 + x3^6;ZZRingElem[511116753300641401,0,86629958186549390,0,6607369692194445,0,298638178178280,0,8857912064610,0,180160923348,0,2544645810,0,24645480,0,156645,0,590,0,1]",zf)
 end
 
 @testset "CPU Costachunks Strategy" begin
