1914822a33bcad8d45472910099ae0aad2513232
Ryan Batubara <ryan.y.batubara@gmail.com>
2026-02-16 23:49:33 -0800

    change r func to call r_m

--- src/ZetaFunction.jl
+++ src/ZetaFunction.jl
@@ -360,33 +360,9 @@ function zeta_function_auto(f; basis=nothing, verbose=0, kwargs...)
 end
 
 function precision_information_user_r(f, r; basis=nothing, verbose=0)
-    p = Int64(characteristic(parent(f)))
     n = nvars(parent(f)) - 1
-    d = total_degree(f)
-
-    if basis === nothing
-        params = default_params()
-        S = collect(0:n)
-        cache = controlled_reduction_cache(n,d,S,params)
-        basis = compute_monomial_bases(f, params, cache)
-        Basis = []
-        for i in 1:n
-            for j in basis[i]
-                push!(Basis,[j,i])
-            end
-        end
-    else
-        Basis = basis
-    end
-
-    hodge_polygon = hodgepolygon(Basis, n)
-    hodge_numbers = hodge_polygon.slopelengths
-    r_m = fill(Int(r), length(hodge_numbers))
-    N_m = series_precision(p,n,d,r_m)
-    N_m[N_m .== 0] .= 1
-    M = algorithm_precision(p,n,d,r_m,N_m)
-
-    return (hodge_polygon, r_m, N_m, M)
+    r_m = fill(Int(r), n)
+    return precision_information_user_r_m(f, r_m; basis=basis, verbose=verbose)
 end
 
 function precision_information_user_r_m(f, r_m; basis=nothing, verbose=0)
@@ -425,18 +401,9 @@ function precision_information_user_r_m(f, r_m; basis=nothing, verbose=0)
 end
 
 function precision_information_max_auto_r(f, r; basis=nothing, verbose=0)
-    (hodge_polygon, r_m_auto, _, _) = auto_precision(f; basis=basis, verbose=verbose)
-    (_, r_m_user, _, _) = precision_information_user_r(f, r; basis=basis, verbose=verbose)
-
-    p = Int64(characteristic(parent(f)))
     n = nvars(parent(f)) - 1
-    d = total_degree(f)
-    r_m = max.(r_m_auto, r_m_user)
-    N_m = series_precision(p,n,d,r_m)
-    N_m[N_m .== 0] .= 1
-    M = algorithm_precision(p,n,d,r_m,N_m)
-
-    return (hodge_polygon, r_m, N_m, M)
+    r_m = fill(Int(r), n)
+    return precision_information_max_auto_r_m(f, r_m; basis=basis, verbose=verbose)
 end
 
 function precision_information_max_auto_r_m(f, r_m; basis=nothing, verbose=0)
@@ -459,8 +426,8 @@ Input f, constant r for all r_m for precision.
 Then take maximum of auto and user input.
 """
 function zeta_function_max_auto_r(f, r; basis=nothing, verbose=0, kwargs...)
-    precision_info = precision_information_max_auto_r(f, r; basis=basis, verbose=verbose)
-    return zeta_function(f; precision_info=precision_info, verbose=verbose, kwargs...)
+    r_m = fill(Int(r), nvars(parent(f)) - 1)
+    return zeta_function_max_auto_r_m(f, r_m; basis=basis, verbose=verbose, kwargs...)
 end
 
 """
@@ -476,12 +443,8 @@ end
 Input f, constant r for all r_m for precision.
 """
 function frobenius_matrix_user_r(f, r; basis=nothing, verbose=0, kwargs...)
-    precision_info = precision_information_user_r(f, r; basis=basis, verbose=verbose)
-    result = zeta_function(f; precision_info=precision_info, givefrobmat=true, verbose=verbose, kwargs...)
-    if result == false
-        return false
-    end
-    return result[1]
+    r_m = fill(Int(r), nvars(parent(f)) - 1)
+    return frobenius_matrix_user_r_m(f, r_m; basis=basis, verbose=verbose, kwargs...)
 end
 
 """
