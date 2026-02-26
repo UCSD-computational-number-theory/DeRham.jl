2e403e18200a8acdd22273faee883c17863289d8
Steve Huang <128733600+stevehuang235@users.noreply.github.com>
2026-02-26 14:02:00 -0500

    Update ZetaFunction.jl

--- src/ZetaFunction.jl
+++ src/ZetaFunction.jl
@@ -665,7 +665,7 @@ INPUTS:
 * "p" -- a prime number, integer 
 
 """
-function zeta_function(f, p; S=[-1], verbose=0, changef=true, givefrobmat=false, algorithm=:naive, termorder=:invlex, vars_reversed=false, fastevaluation=false, always_use_bigints=false, use_gpu=false, use_threads=false, context=nothing)
+function zeta_function(f, p; S=[-1], verbose=0, changef=true, givefrobmat=false, algorithm=:default, termorder=:invlex, vars_reversed=false, fastevaluation=false, always_use_bigints=false, use_gpu=false, use_threads=false, context=nothing)
     @assert is_prime(p) "p must be prime"
     PR = parent(f)
     PRmodp, hom = change_base_ring(GF(p), PR)
@@ -673,7 +673,7 @@ function zeta_function(f, p; S=[-1], verbose=0, changef=true, givefrobmat=false,
     return zeta_function(hom(f);S=S, verbose=verbose, changef=changef, givefrobmat=givefrobmat, algorithm=algorithm, termorder=termorder, vars_reversed=vars_reversed, fastevaluation=fastevaluation, always_use_bigints=always_use_bigints, use_gpu=use_gpu, use_threads=use_threads, context=context)
 end 
 
-function newton_polygon(f; S=[-1], verbose=0, changef=true, algorithm=:naive, termorder=:invlex, vars_reversed=false, fastevaluation=true, always_use_bigints=false, use_gpu=false)
+function newton_polygon(f; S=[-1], verbose=0, changef=true, algorithm=:default, termorder=:invlex, vars_reversed=false, fastevaluation=true, always_use_bigints=false, use_gpu=false)
     PR = parent(f)
     R = coefficient_ring(PR)
     p = Int64(characteristic(PR))
